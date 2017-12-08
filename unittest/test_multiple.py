37583cf60f0ac6af666222d2112be941eb9b03a4
Szczepan Zalega <szczepan@nitrokey.com>
2017-12-08 11:48:26 +0100

    WIP add python test
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

---
+++ unittest/test_multiple.py
@@ -0,0 +1,45 @@
+import pprint
+from time import sleep
+
+import pytest
+
+from conftest import skip_if_device_version_lower_than
+from constants import DefaultPasswords, DeviceErrorCode, bb
+from misc import gs, wait
+
+pprint = pprint.PrettyPrinter(indent=4).pprint
+
+
+@pytest.mark.other
+@pytest.mark.info
+def test_get_status_storage_multiple(C):
+    ids = gs(C.NK_list_devices_by_cpuID())
+    print(ids)
+    devices_list = ids.split(b';')
+    #
+    # for s in devices_list:
+    #     res = C.NK_connect_with_ID(s)
+    #     assert res == 1
+    #     # st = gs(C.NK_get_status_storage_as_string())
+    #     # print(len(st))
+    #     C.NK_lock_device()
+    #
+    # for s in devices_list:
+    #     res = C.NK_connect_with_ID(s)
+    #     assert res == 1
+    #     v = C.NK_fill_SD_card_with_random_data(b'12345678')
+    #     print(v)
+
+    devices_count = len(devices_list)
+    assert devices_count != 0
+    b = 0
+    while b/devices_count < 100:
+        b = 0
+        for s in devices_list:
+            res = C.NK_connect_with_ID(s)
+            assert res == 1
+            b += C.NK_get_progress_bar_value()
+        print('{}: {}'.format(b, int(b/devices_count) * '='))
+        sleep(1)
+
+
